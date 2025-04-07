625bd633b6d0bbbd2a2f19bb36a00f7125d3e721
jlsyetsu404040 <ethanbourgi56@gmail.com>
2025-04-07 20:02:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ async def on_ready():
             print("Cog Loaded!")
     print(f'{client.user} has connected to Discord!')
 
-client.run("paste your token here")
+client.run("MTM1ODg0MzgzNTg4MjY2ODI4Ng.GOjRXI.-DYLV3La2yK7d8xIxp_7JvbhA6eMkZt8aJyzi0")
